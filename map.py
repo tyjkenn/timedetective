321f9fca28b16093d6427ef41631fecd584dec93
Ty Kennington <tyjkenn@gmail.com>
2016-11-15 16:04:32 -0700

    reference tiles based on properties instead of gids

--- map.py
+++ map.py
@@ -13,30 +13,37 @@ class Renderer(object):
     def findGround(self):
         global groundPoints, leftTileCount, rightTileCount
         foundGround = False
-        for x in xrange(self.tmx_data.width):
-            foundInCol = False
-            for y in xrange(self.tmx_data.height):
-                gid = self.tmx_data.get_tile_gid(x, y, 3)
-                if gid == 167 or gid == 169:
-                    if groundPoints.count == 0:
-                        if gid == 169:
+        layer_index = 0
+        for layer in self.tmx_data.layers:
+            if layer.name != 'ground':
+                layer_index += 1
+                continue
+            for x in xrange(self.tmx_data.width):
+                foundInCol = False
+                for y in xrange(self.tmx_data.height):
+                    props = self.tmx_data.get_tile_properties(x, y, layer_index)
+                    if props is not None:
+                        slope = props['groundSlope']
+                        if slope == 'flat' or slope == 'down':
+                            if groundPoints.count == 0:
+                                if slope == 'flat':
+                                    groundPoints.append(y * self.tmx_data.tileheight + 16)
+                                else:
+                                    groundPoints.append(y * self.tmx_data.tileheight)
                             groundPoints.append(y * self.tmx_data.tileheight + 16)
-                        else:
+                            foundGround = True
+                            foundInCol = True
+                            break
+                        if slope == 'up':
                             groundPoints.append(y * self.tmx_data.tileheight)
-                    groundPoints.append(y * self.tmx_data.tileheight + 16)
-                    foundGround = True
-                    foundInCol = True
-                    break
-                if gid == 168:
-                    groundPoints.append(y * self.tmx_data.tileheight)
-                    foundGround = True
-                    foundInCol = True
-                    break
-            if not foundInCol:
-                if foundGround:
-                    rightTileCount += 1
-                else:
-                    leftTileCount += 1
+                            foundGround = True
+                            foundInCol = True
+                            break
+                if not foundInCol:
+                    if foundGround:
+                        rightTileCount += 1
+                    else:
+                        leftTileCount += 1
 
     """
     This object renders tile maps from Tiled
