58b7eeadb4d0cfd2209250ffb0fe7ddeb05d834c
ShoujiKanenobu <kyle@mcghie.org>
2016-12-09 11:54:03 -0700

    Added end and win screen

--- graphics.py
+++ graphics.py
@@ -15,6 +15,8 @@ _font = None
 
 _entities = []
 
+userInput = 0
+
 #Dialog Variables
 talking = False
 optionPhase = False
@@ -68,6 +70,51 @@ def intro():
     for i in xrange(len(lines)):
         label = (_font.render(lines[i], 1, (255,255,255)))
         _screen.blit(label, (30, 100 + (i*20)))
+    pygame.display.flip();
+
+def endScreen():
+    global _screen, _font, userInput
+    _screen.fill((0,0,0))
+    text = "Who do you think is the murderer?"
+    lines = textwrap.wrap(text, 35)
+    lines.append("1. Hobo")
+    lines.append("2. Artist")
+    lines.append("3. Chef")
+    lines.append("4. Innkeeper")
+    lines.append("5. Huntress")
+    lines.append("6. Old Man")
+    lines.append("7. Scientist")
+    lines.append("8. Gardener")
+    lines.append("9. Merchant")
+    i = 0
+    for theBot in botManager.bots:
+        i += 1
+        if theBot.role == "Murderer":
+            Correct = i
+            print Correct
+    if userInput == 0:
+        lines.append("Guess the murderer...")
+    elif userInput == Correct:
+        lines.append("Correct")
+        return "win"
+    elif userInput != Correct:
+        lines.append("Wrong...")
+        return "start"
+    for i in xrange(len(lines)):
+        label = (_font.render(lines[i], 1, (255,255,255)))
+        _screen.blit(label, (30, 50 + (i*20)))
+    pygame.display.flip();
+    userInput = 0
+    return "end"
+
+def win():
+    global _screen, _font
+    _screen.fill((0,0,0))
+    text = "You win!"
+    lines = textwrap.wrap(text, 35)
+    for i in xrange(len(lines)):
+        label = (_font.render(lines[i], 1, (255,255,255)))
+        _screen.blit(label, (150, 150 + (i*20)))
     pygame.display.flip();
 
 def update():
